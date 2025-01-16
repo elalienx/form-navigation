00b23698d6f33b31308092376827a83ca8c64d01
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-01-16 12:13:48 +0100

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Form navigator</h1>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// Node modules
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
