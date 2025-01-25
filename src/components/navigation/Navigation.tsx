f453a71d7373c14fa76b744cb2053c1e70a8722a
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-01-25 13:44:17 +0100

    refactor: finish implementing the fined grained navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -33,7 +33,7 @@ export default function Navigation() {
           onClick={() => onItemClick(data[1].id)}
           isActive={data[1].id === stepId}
         />
-        {/* Three */}
+        {/* Three (special) */}
         {hasCoApplicant ? (
           <ItemMenu item={data[2]} />
         ) : (
@@ -43,6 +43,72 @@ export default function Navigation() {
             isActive={data[2].id === stepId}
           />
         )}
+        {/* Four */}
+        {hasCoApplicant ? (
+          <ItemSecondary
+            item={data[3]}
+            onClick={() => onItemClick(data[3].id)}
+            isActive={data[3].id === stepId}
+          />
+        ) : null}
+        {/* Five */}
+        {hasCoApplicant ? (
+          <ItemSecondary
+            item={data[4]}
+            onClick={() => onItemClick(data[4].id)}
+            isActive={data[4].id === stepId}
+          />
+        ) : null}
+        {/* Six (special) */}
+        {hasCoApplicant ? (
+          <ItemMenu item={data[5]} />
+        ) : (
+          <ItemPrimary
+            item={data[5]}
+            onClick={() => onItemClick(data[5].id)}
+            isActive={data[5].id === stepId}
+          />
+        )}
+        {/* Seven */}
+        {hasCoApplicant ? (
+          <ItemSecondary
+            item={data[6]}
+            onClick={() => onItemClick(data[6].id)}
+            isActive={data[6].id === stepId}
+          />
+        ) : null}
+        {/* Eight */}
+        {hasCoApplicant ? (
+          <ItemSecondary
+            item={data[7]}
+            onClick={() => onItemClick(data[7].id)}
+            isActive={data[7].id === stepId}
+          />
+        ) : null}
+        {/* Nine */}
+        <ItemPrimary
+          item={data[8]}
+          onClick={() => onItemClick(data[8].id)}
+          isActive={data[8].id === stepId}
+        />
+        {/* Ten */}
+        <ItemPrimary
+          item={data[9]}
+          onClick={() => onItemClick(data[9].id)}
+          isActive={data[9].id === stepId}
+        />
+        {/* Eleven */}
+        <ItemPrimary
+          item={data[10]}
+          onClick={() => onItemClick(data[10].id)}
+          isActive={data[10].id === stepId}
+        />
+        {/* Twelve */}
+        <ItemPrimary
+          item={data[11]}
+          onClick={() => onItemClick(data[11].id)}
+          isActive={data[11].id === stepId}
+        />
       </ul>
     </nav>
   );
